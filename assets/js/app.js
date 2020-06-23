c2017d7f934de917b4659a62cd205819f5de6e99
Stivens Espinoza Reina <stivens70espinoza@gmail.com>
2020-06-23 12:58:05 -0500

    Estructura carga musica

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const btnReaccion=document.getElementById('reaccion');
+const contenedorListaMusic=document.getElementById('lista-music');
+const controles=document.getElementById('controles');
+
+const menuMusic=document.getElementById('menuMusic');
+const titlePlaylist=document.getElementById('titlePlaylist');
+const playDescription=document.getElementById('playDescription');
+const imgAlbum=document.getElementById('imgAlbum');
+const album=document.getElementById('album');
